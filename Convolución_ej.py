dfb02de196345e11540eed9f26aac43eb1aafaec
EmilianoV-TEC <A01707035@itesm.mx>
2021-05-05 07:40:13 -0800

    Arregla uso incorrecto de filas/columnas

--- Convolución_ej.py
+++ Convolución_ej.py
@@ -8,11 +8,12 @@ import numpy as np
 def conv_mat(matriz, kernel):
 	fil_m, col_m = matriz.shape
 	fil_k, col_k = kernel.shape
-	conv_final = np.zeros(((fil_m-fil_k)+1, (fil_m-fil_k)+1)) #Se declara matriz con dimensiones finales
+	conv_final = np.zeros(((fil_m-fil_k)+1, (col_m-col_k)+1)) #Se declara matriz con dimensiones finales
 	for i in range(conv_final.shape[0]):
 		for j in range(conv_final.shape[1]):
 			producto = np.multiply(matriz[i:i+fil_k, j:j+col_k], kernel)
 			conv_final[i][j] = np.sum(producto)
+
 	return(conv_final)
 
 
