cb28aac4fe92b3576086a214e643b05b5b3e4e9c
EmilianoV-TEC <A01707035@itesm.mx>
2021-05-05 12:05:22 -0800

    Agrega implementación de padding

--- Avances Convolución/Convolución_ej.py
+++ Avances Convolución/Convolución_ej.py
@@ -1,16 +1,19 @@
 """
-Programa para realizar la convolución de una imagen, sin padding y stride de 1
+Programa para realizar la convolución de una matriz, con padding definido y stride de 1
+Por Emiliano Vásquez Olea
 """
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+from add_padding import add_padding
 
-def conv_mat(matriz, kernel):
+def conv_mat(matriz, kernel, padding):
+	matriz = add_padding(matriz, padding)
 	fil_m, col_m = matriz.shape
 	fil_k, col_k = kernel.shape
 	conv_final = np.zeros(((fil_m-fil_k)+1, (col_m-col_k)+1)) #Se declara matriz con dimensiones finales
+
 	for i in range(conv_final.shape[0]):
 		for j in range(conv_final.shape[1]):
 			producto = np.multiply(matriz[i:i+fil_k, j:j+col_k], kernel)
@@ -18,7 +21,7 @@ def conv_mat(matriz, kernel):
 
 	return(conv_final)
 
-def filtro_img(imagen, kernel): #Función para aplicar filtro y mostrar imagenes
+def filtro_img(imagen, kernel, padding): #Función para aplicar filtro y mostrar imagenes
 	plt.imshow(cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB))
 	plt.title("Imagen original")
 	plt.show()
@@ -31,11 +34,13 @@ def filtro_img(imagen, kernel): #Función para aplicar filtro y mostrar imagenes
 	plt.title("Imagen en escala de grises")
 	plt.show()
 
-	imagen_conv = conv_mat(imagen, kernel)
+	imagen_conv = conv_mat(imagen, kernel, padding)
 
 	plt.imshow(imagen_conv, cmap='gray')
 	plt.title("Imagen con filtro aplicado")
 	plt.show()
+
+	return(imagen_conv)
 
 """
 Se utiliza kernel de ejemplo para probar el código (box blur)
@@ -44,9 +49,12 @@ ej_kernel = np.array([[1/9, 1/9, 1/9],
 					  [1/9, 1/9, 1/9],
 					  [1/9, 1/9, 1/9]])
 
+# Se leen argumentos desde la línea de comandos como "python Convolución_ej.py -i Img_Casa.jpg"
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Ruta a imagen")
 args = vars(ap.parse_args())
 imagen = cv2.imread(args["image"]) 
-imagen = filtro_img(imagen, ej_kernel)
+#Se lee el valor para el padding y posteriormente se muestran las imágenes
+p = int(input("Inserta el valor para el padding: "))
+imagen = filtro_img(imagen, ej_kernel, p)
 
