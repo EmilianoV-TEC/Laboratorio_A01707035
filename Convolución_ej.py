f9796d43f3908310361ad575f3354bf69f5c75de
EmilianoV-TEC <A01707035@itesm.mx>
2021-05-05 11:13:50 -0800

    Agrega manejo de imagenes con matplotlib

--- Convolución_ej.py
+++ Convolución_ej.py
@@ -1,9 +1,11 @@
 """
-Programa para realizar la convolución de una matriz, sin padding y stride de 1
+Programa para realizar la convolución de una imagen, sin padding y stride de 1
 """
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import argparse
 
 def conv_mat(matriz, kernel):
 	fil_m, col_m = matriz.shape
@@ -16,19 +18,35 @@ def conv_mat(matriz, kernel):
 
 	return(conv_final)
 
+def filtro_img(imagen, kernel): #Función para aplicar filtro y mostrar imagenes
+	plt.imshow(cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB))
+	plt.title("Imagen original")
+	plt.show()
+
+	if len(imagen.shape) == 3:
+		imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+		print("Imagen cambiada a escala de grises")
+
+	plt.imshow(imagen, cmap='gray')
+	plt.title("Imagen en escala de grises")
+	plt.show()
+
+	imagen_conv = conv_mat(imagen, kernel)
+
+	plt.imshow(imagen_conv, cmap='gray')
+	plt.title("Imagen con filtro aplicado")
+	plt.show()
 
 """
-Se utilizan matrices de ejemplo para probar el código
+Se utiliza kernel de ejemplo para probar el código (box blur)
 """
-ej_matriz = np.array([[10, 4, 50, 30, 20], 
-				 	  [80, 0, 0, 0, 6],
-				 	  [0, 0, 1, 16, 17],
-				 	  [0, 1, 0, 7, 23],
-				 	  [1, 0, 6, 0, 4]])
+ej_kernel = np.array([[1/9, 1/9, 1/9],
+					  [1/9, 1/9, 1/9],
+					  [1/9, 1/9, 1/9]])
 
-ej_kernel = np.array([[1, 0, 1],
-					  [0, 0, 0],
-					  [1, 0, 3]])
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Ruta a imagen")
+args = vars(ap.parse_args())
+imagen = cv2.imread(args["image"]) 
+imagen = filtro_img(imagen, ej_kernel)
 
-final = conv_mat(ej_matriz, ej_kernel)
-print(final)
